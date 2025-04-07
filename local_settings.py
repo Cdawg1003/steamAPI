02cb18412b0a4613347ff5724dd3d2a4b7ce1281
Cdawg1003 <sebastianhm1003@gmail.com>
2025-04-07 09:58:58 -0700

    First commit

---
+++ local_settings.py
@@ -0,0 +1,6 @@
+postgresql = {'pguser': 'postgres', 
+              'pgpasswd': 'CbassEngineering1003.', 
+              'pghost': 'localhost', 
+              'pgport': 5432, 
+              'pgdb': 'steam_games_details'
+              }
